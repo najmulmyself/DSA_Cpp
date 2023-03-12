928d9a41fb9c3458e1b10878641956b219f614f6
najmulmyself <najmul.myself@gmail.com>
2023-03-13 01:32:24 +0600

    need to fix binary search

--- lab 01/kthBinaryS.cpp
+++ lab 01/kthBinaryS.cpp
@@ -1,31 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void binary_search(vector<int> a,int k){
+int binary_search(vector<int> a,int k){
     int left = 0;
     int right = a.size()-1;
+    int count =0;
 
 
     while (left<=right)
     {   
     int mid = left+right/2;
         if(k == a[mid]){
+            count++;
             cout<< "found mid at line 13";
+            if(a[mid+1]== k){
+                count++;
+                break;
+            }
         }
-        else if(k<a[mid]){
+         if(k<a[mid]){
             right = mid-1;
         }
         else{
             left = mid +1;
         }
     }
-    
+    return count;
 }
 
 int main(){
-    vector<int> a = {1,3,4,5,6,6,9,17};
-    int k = 6;
+    // vector<int> a = {1,3,4,5,6,6,9,17};
+    vector<int> a = {1, 2, 3,4, 4, 5, 6, 7, 8, 9 ,10};
+    int k = 4;
 
-    binary_search(a,k);
+    int count = binary_search(a,k);
+    if(count > 1){
+        cout<<"Yes"<<count;
+
+        }else {
+            cout<<"NO";
+    }
+
     return 0;
 }
