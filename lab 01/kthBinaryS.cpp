34397f7e398c58185b0185e89bcf27af570b92c3
najmulmyself <najmul.myself@gmail.com>
2023-03-15 01:42:04 +0600

    solved binary search

--- lab 01/kthBinaryS.cpp
+++ lab 01/kthBinaryS.cpp
@@ -1,44 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binary_search(vector<int> a,int k){
-    int left = 0;
-    int right = a.size()-1;
-    int count =0;
-
-
-    while (left<=right)
-    {   
-    int mid = left+right/2;
-        if(k == a[mid]){
-            count++;
-            cout<< "found mid at line 13";
-            if(a[mid+1]== k){
-                count++;
-                break;
-            }
-        }
-         if(k<a[mid]){
-            right = mid-1;
-        }
-        else{
-            left = mid +1;
+int findFirstOccurrence(vector<int>& arr, int k) {
+    int n = arr.size();
+    int low = 0, high = n - 1, result = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == k) {
+            result = mid;
+            high = mid - 1;
+        } else if (arr[mid] < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
-    return count;
+    return result;
 }
 
-int main(){
-    // vector<int> a = {1,3,4,5,6,6,9,17};
-    vector<int> a = {1, 2, 3,4, 4, 5, 6, 7, 8, 9 ,10};
-    int k = 4;
+int findLastOccurrence(vector<int>& arr, int k) {
+    int n = arr.size();
+    int low = 0, high = n - 1, result = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == k) {
+            result = mid;
+            low = mid + 1;
+        } else if (arr[mid] < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return result;
+}
 
-    int count = binary_search(a,k);
-    if(count > 1){
-        cout<<"Yes"<<count;
+int main() {
+    int n;
+    cin >> n;
 
-        }else {
-            cout<<"NO";
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cin >> k;
+
+    int first = findFirstOccurrence(arr, k);
+    int last = findLastOccurrence(arr, k);
+
+    if (first != -1 && last != -1 && first != last) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
     }
 
     return 0;
