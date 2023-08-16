d54a0e98456e8931b555ed79594a9ec111fd5749
najmulmyself <najmul.myself@gmail.com>
2023-08-16 22:09:28 +0600

    solved one

--- revisionEx/reverseArr.cpp
+++ revisionEx/reverseArr.cpp
@@ -1,10 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> reverseArr(vector<int> arr){
-    if(arr.size() == 0)
+vector<int> reverseArr(vector<int> arr)
+{
+    for (int i = 0; i < arr.size() / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.size() - 1 - i];
+        arr[arr.size() - 1 - i] = temp;
+    }
+    return arr;
 }
-int main(){
-    
+int main()
+{
+    int size;
+    vector<int> arr;
+
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+
+    vector<int> revArr = reverseArr(arr);
+
+    for (int i = 0; i < revArr.size(); i++)
+    {
+        cout << revArr[i] << " ";
+    }
     return 0;
 }
